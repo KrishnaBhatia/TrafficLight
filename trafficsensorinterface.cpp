6c6690c363e7dd1a4744a3805510ac6f8877a23c
Krishna <29398810+KrishnaBhatia@users.noreply.github.com>
2023-08-15 13:31:17 -0700

    Add files via upload

---
+++ trafficsensorinterface.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file trafficsensorinterface.cpp
+ * @author Krishna Bhatia
+ * @brief Class implementation for TrafficSensorInterface
+*/
+
+#include "trafficsensorinterface.hpp"
+
+Direction TrafficSensorInterface::GetDirection() const {
+   // Get direction
+}
+
+bool TrafficSensorInterface::HasWaitingVehicles() const {
+   // Check if sensor detects waiting cars
+}
+
+bool TrafficSensorInterface::IsValid() const {
+   // Perform self-diagnostics and return status
+}
